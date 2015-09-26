d4a0df047912730efbb4f3178238d861d32dd248
François Massot <francois.massot@gmail.com>
2015-09-26 12:21:49 +0200

    Remove absolute path of sofficeº

--- convert_docs_to_html.sh
+++ convert_docs_to_html.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ls source-doc | while read file; do
-  /Applications/LibreOffice.app/Contents/MacOS/soffice --headless  --convert-to html "source-doc/$file"
+  soffice --headless  --convert-to html "source-doc/$file"
   html=$(echo "$file" | sed 's/.doc/.html/')
   echo $file $html
   mv "$html" "html/$html"
