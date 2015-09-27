6dd9df6336ef32960459ad0577d66248197281eb
Tangui Morlier <tangui@tangui.eu.org>
2015-09-27 14:52:12 +0200

    Merge companies & streets in a commun python

--- arretes/find_metas.py
+++ arretes/find_metas.py
@@ -13,7 +13,6 @@ re_lieu_dit = re.compile(ur'(lieu-dit|lieux-dits|lieudit)')
 
 GEOCODE_CACHE = {}
 
-
 def load_streetnames():
     renames = []
     with open('voies.txt', 'r') as input:
@@ -43,12 +42,8 @@ def find_poi_from_street_id(data):
     return streets
 
 
-def find_poi(filename):
-    with open(filename, 'r') as input:
-        data = json.load(input, encoding='utf-8')
+def find_poi(data):
         streets = list(set(find_poi_from_street_id(data) + find_poi_from_streetnames(data)))
-
-        data['meta'] = {'streets': []}
         for street in streets:
             if street not in GEOCODE_CACHE:
                 GEOCODE_CACHE[street] = geocode(street)
@@ -62,11 +57,35 @@ def find_poi(filename):
                     'name': result['properties']['name']
                 })
 
-        with open(filename.replace('json/', 'json/geocoded_'), 'w') as output:
-            json.dump(data, output, encoding="utf-8")
-                
+        return data
+
+re_personnes_morales = re.compile(ur'(sociétés?|entreprises?|associations?) ?([^,\.:]+)', re.I)
+re_suppr_phrase = re.compile(ur'( est| sont| et| dès| chargé| à| \d| de | TP| SAG| SARL| agence| ouest| ouets).*', re.I)
+re_exclude = re.compile(ur'(personne physique|\()')
+
+def find_personnes_morales(data):
+    companies = {}
+    for article in data['articles']:        
+        m = re_personnes_morales.search(article)
+        if (m):
+            if not re_exclude.search(m.group(2)):
+                companies[m.group(2)] = m.group(1)
+    for companie in companies:
+        if  'companies' not in data['meta']:
+            data['meta']['companies'] = []            
+        data['meta']['companies'] += {"nom": companie, "type": companies[companie]}
+    return data
+
+def general_find(filename):
+    with open(filename, 'r') as input:
+        data = json.load(input, encoding='utf-8')
+        data['meta'] = {'streets': []}
+        data = find_poi(data)
+        data = find_personnes_morales(data)
+    with open(filename.replace('json/', 'json/meta_'), 'w') as output:
+        json.dump(data, output, encoding="utf-8")
 
 if __name__ == '__main__':
     for arg in sys.argv[1:]:
         print arg
-        find_poi(arg)
+        general_find(arg)
