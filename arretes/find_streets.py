bcd576e783d91575d5c70647d6beab5d0d5a8380
Tangui Morlier <tangui@tangui.eu.org>
2015-09-27 12:10:08 +0200

    from streetnames

--- arretes/find_streets.py
+++ arretes/find_streets.py
@@ -3,17 +3,34 @@
 
 import sys
 import json
+import re
 
-
-def find_streets(filename):
-    with open(filename, 'r') as input:
+def load_streetnames():
+    renames = []
+    with open('voies.txt', 'r') as input:
         for line in input.readlines():
-            line.strip()
+            str = ur'(\w+ '+line.strip()+')'
+            renames.append(re.compile(str, re.I))
+    return renames
 
+
+
+def find_pio_from_streetnames(filename):
+    with open(filename, 'r') as input:
         data = json.load(input, encoding='utf-8')
 
+        print data['articles']
+        
+        re_streetnames = load_streetnames()
+        
         for article in data['articles']:
-            pass
+            for p in re_streetnames:
+                m = p.search(article)
+                if (m):
+                    print m.group()
 
+def find_streets(filename):
+    return find_pio_from_streetnames(filename)
+                
 if __name__ == '__main__':
     print json.dumps(find_streets(sys.argv[1]), ensure_ascii=False).encode('utf-8')
